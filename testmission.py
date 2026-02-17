843986394ce14cf2caa9416fedebe5169411c4d7
Thomas A <thomas.aftandilian@gmail.com>
2026-02-16 16:14:09 -0800

    Refactor motor assignments for drive and arm control

--- testmission.py
+++ testmission.py
@@ -6,45 +6,46 @@ from pybricks.parameters import Port, Direction
 from pybricks.robotics import DriveBase
 
 hub = PrimeHub()
-left_motor = Motor(Port.C)
-arm_motor = Motor(Port.A)
-right_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+leftdrive_motor = Motor(Port.E)
+leftarm_motor = Motor(Port.B)
+rightarm_motor = Motor(Port.D)
+rightdrive_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 use_gyro = True
 
-drive_base = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=100)
+drive_base = DriveBase(leftdrive_motor, rightdrive_motor, wheel_diameter=56, axle_track=100)
 hub.imu.reset_heading(0)
 global global_angle
 global_angle = 0
 def drive(speed):
-    left_motor.run(speed)
-    right_motor.run(speed)
-    print("I WAS HERE")
+    leftdrive_motor.run(speed)
+    rightdrive_motor.run(speed)
+    #print("I WAS HERE")
 def turnwithgyro(target_angle, turn_speed):
     global global_angle
     starting_yaw = hub.imu.heading()
     global_angle += target_angle
     
     if(target_angle>0):
-        left_motor.run(-turn_speed)
-        right_motor.run(turn_speed)
+        leftdrive_motor.run(-turn_speed)
+        rightdrive_motor.run(turn_speed)
         while True:
             current = hub.imu.heading()
-            if current >= global_angle:
+            if current >= global_angle-3:
                 break
     else:
-        left_motor.run(turn_speed)
-        right_motor.run(-turn_speed)
+        leftdrive_motor.run(turn_speed)
+        rightdrive_motor.run(-turn_speed)
         while True:
             current = hub.imu.heading()
-            if current <= global_angle:
+            if current <= global_angle+3:
                 break
-    left_motor.stop()
-    right_motor.stop()
+    leftdrive_motor.stop()
+    rightdrive_motor.stop()
 
 
 drive_base.straight(300)
-turnwithgyro(90,200)
-arm_motor.run_angle(500, 300)
+turnwithgyro(90,100)
+leftarm_motor.run_angle(500, 300)
 drive_base.straight(150)
 drive_base.straight(-450)
 
