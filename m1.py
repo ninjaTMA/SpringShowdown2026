e921307cc6b59dbd7e62a1a1385333ebcb4b7adb
Thomas A <thomas.aftandilian@gmail.com>
2026-03-27 20:16:18 -0700

    Create m1.py

---
+++ m1.py
@@ -0,0 +1,70 @@
+#Mission Code for The Silo And Forge
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Axis
+from pybricks.tools import wait
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+from pybricks.tools import multitask, run_task
+
+hub = PrimeHub()
+leftdrive_motor = Motor(Port.E)
+leftarm_motor = Motor(Port.B)
+rightarm_motor = Motor(Port.F)
+rightdrive_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+use_gyro = True
+
+drive_base = DriveBase(leftdrive_motor, rightdrive_motor, wheel_diameter=62.5, axle_track=100)
+drive_base.settings(straight_speed=800, straight_acceleration=800, turn_rate=100, turn_acceleration=900)
+
+hub.imu.reset_heading(0)
+global global_angle
+global_angle = 0
+
+async def turnwithgyro(target_angle, turn_speed):
+    global global_angle
+    starting_yaw = hub.imu.heading()
+    global_angle += target_angle
+    
+    if(target_angle>0):
+        leftdrive_motor.run(-turn_speed)
+        rightdrive_motor.run(turn_speed)
+        while True:
+            current = hub.imu.heading()
+            if current >= global_angle-3:
+                break
+    else:
+        leftdrive_motor.run(turn_speed)
+        rightdrive_motor.run(-turn_speed)
+        while True:
+            current = hub.imu.heading()
+            if current <= global_angle+3:
+                break
+    leftdrive_motor.stop()
+    rightdrive_motor.stop()
+
+    
+
+
+
+### EXAMPLE CODE ###
+###drive_base.straight(300)
+###turnwithgyro(90,100)
+###leftarm_motor.run_angle(500, 300)
+###drive_base.straight(150)
+###drive_base.straight(-450)*/
+
+async def main():
+    #move forward and raise arm at the same time
+    await multitask(drive_base.straight(395), rightarm_motor.run_angle(500, 300))
+    #wait for boulders to fall out
+    wait(1667)
+    #collect all boudlers
+    await drive_base.straight(065)
+    #drive back and wait
+    await drive_base.straight(-345)
+
+run_task(main())
+
+
+
