b5f1a9b009a2546295871ff3c7ccb1542f433cc0
zhujacobj <zhujacobj@gmail.com>
2026-05-11 16:14:04 -0700

    removed m7

--- finalmissions.py
+++ finalmissions.py
@@ -1,7 +1,7 @@
 from pybricks.tools import hub_menu
 
 # Make a menu to choose a letter. You can also use numbers.
-selected = hub_menu("1", "2", "3", "4", "5", "6", "7")
+selected = hub_menu("1", "2", "3", "4", "5", "6")
 
 # Based on the selection, run a program.
 if selected == "1":
