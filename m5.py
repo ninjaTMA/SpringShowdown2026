930c8416c4e1e6a8656526f96421da531b2da70a
zhujacobj <zhujacobj@gmail.com>
2026-05-16 11:47:24 -0700

    Incorporated new arc function and finalized code

--- m5.py
+++ m5.py
@@ -125,18 +125,18 @@ async def tank_turn(hub, motor_left, motor_right, target_degrees, speed):
 async def main():
     drive_base.settings(straight_speed=200, straight_acceleration=200, turn_rate=70, turn_acceleration=70)
     # Pass jig wall
-    await drive_base.straight(50)
+    await drive_base.straight(130)
     # Curve, hit the relic off the pedestal and face temple gates
-    await drive_base.arc(320, 130)
+    await turn_arc(hub, leftdrive_motor, rightdrive_motor, turning_radius=300, turning_degrees=-90, turning_speed=20)
     # Approach temple gates and drop off lantern
-    await drive_base.straight(40)
+    await drive_base.straight(100)
     # Back up to brush mission
     await drive_base.straight(-180)
     # Retrieve the brush
     await leftarm_motor.run_angle(1500, 1300)
     await leftarm_motor.run_angle(1100, -1000)
     # Back up to the pedestal
-    await drive_base.straight(-55)
+    await drive_base.straight(-50)
     # Drop off the idol
     await rightarm_motor.run_angle(1100, -500)
     # Back up a bit to confirm that the idol is on the pedestal
