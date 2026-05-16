f6f2cc3a759b79968a78b8ce2e0b25fed6c1ea13
Thomas A <thomas.aftandilian@gmail.com>
2026-05-16 13:35:24 -0700

    m6 upgrade

--- m6.py
+++ m6.py
@@ -47,18 +47,18 @@ async def turnwithgyro(target_angle, turn_speed):
 
 async def main():
     
-    await multitask(rightarm_motor.run_angle(300, 240), drive_base.straight(410))
+    await multitask(rightarm_motor.run_angle(300, 300), drive_base.straight(390))
 
-    drive_base.settings(straight_speed=500, straight_acceleration=800, turn_rate=50, turn_acceleration=50)
-    await turnwithgyro(-45, 60)
+    drive_base.settings(straight_speed=600, straight_acceleration=800, turn_rate=50, turn_acceleration=50)
+    await turnwithgyro(-45, 100)
    
-    await drive_base.straight(130)
+    await drive_base.straight(150)
     wait(1000)
     await drive_base.straight(-145)
     await rightarm_motor.run_angle(-400, 200)
-    await turnwithgyro(-12, 80)
+    await turnwithgyro(-12, 140)
     await drive_base.straight(190)
-    await turnwithgyro(95, 80)
+    await turnwithgyro(95, 140)
     await drive_base.straight(10)
     await leftarm_motor.run_angle(-400, 300)
     
