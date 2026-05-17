3cd8e496b0ae564f103b1a1971150a3bf778afd2
Thomas A <thomas.aftandilian@gmail.com>
2026-05-16 20:44:02 -0700

    added tokens

--- m6new.py
+++ m6new.py
@@ -48,7 +48,13 @@ async def turnwithgyro(target_angle, turn_speed):
 
 async def main():
     await drive_base.straight(350)
-    await turnwithgyro(20, 100)
+    await turnwithgyro(28, 100)
     await drive_base.straight(130)
     await wait(500)
+    await drive_base.straight(-250)
+    await turnwithgyro(-48, 100)
+    await drive_base.straight(100)
+    await turnwithgyro(48, 200)
+    await drive_base.straight(150)
+    await leftarm_motor.run_angle(500, -250)
 run_task(main())
